483a4c2bc06cf7030503a017d096c35e7c5e28ec
emanuel2718 <eramirez2718@gmail.com>
2020-04-05 08:37:48 -0400

    Push init.lua

---
+++ init.lua
@@ -0,0 +1,93 @@
+--Author: Emanuel Ramirez
+--Date: 04/05/2020
+
+function init()
+    appsWatcher = hs.application.watcher.new(previewWatcher)
+    appsWatcher:start()
+end
+
+function previewWatcher(name, event, app)
+    --If preview application is beign focused watch for VIM keybinds
+    if (name == "Preview" and event == hs.application.watcher.activated) then
+
+        --Scroll Left
+        hs.hotkey.bind({}, "H",
+        function()
+            hs.eventtap.scrollWheel({2, 0}, {})
+        end,nil,
+        function()
+            hs.eventtap.scrollWheel({2, 0}, {})
+        end)
+
+        --Scroll Down
+        hs.hotkey.bind({}, "J",
+        function()
+            hs.eventtap.scrollWheel({0, -2}, {})
+        end,nil,
+        function()
+            hs.eventtap.scrollWheel({0, -2}, {})
+        end)
+
+        --Scroll Up
+        hs.hotkey.bind({}, "K",
+        function()
+            hs.eventtap.scrollWheel({0, 2}, {})
+        end,nil,
+        function()
+            hs.eventtap.scrollWheel({0, 2}, {})
+        end)
+
+        --Scroll Right
+        hs.hotkey.bind({}, "L",
+        function()
+            hs.eventtap.scrollWheel({-2, 0}, {})
+        end,nil,
+        function()
+            hs.eventtap.scrollWheel({-2, 0}, {})
+        end)
+    end
+
+    if (name == "Preview" and event == hs.application.watcher.deactivated) then
+        
+        --Disbale Scroll Left Hotkey
+        hs.hotkey.disableAll({}, "H",
+        function()
+            hs.eventtap.scrollWheel({2, 0}, {})
+        end,nil,
+        function()
+            hs.eventtap.scrollWheel({2, 0}, {})
+        end)
+       
+        --Disbale Scroll Down Hotkey
+        hs.hotkey.disableAll({}, "J",
+        function()
+            hs.eventtap.scrollWheel({0, -2}, {})
+        end,nil,
+        function()
+            hs.eventtap.scrollWheel({0, -2}, {})
+        end)
+
+        --Disbale Scroll Up Hotkey
+        hs.hotkey.disableAll({}, "K",
+        function()
+            hs.eventtap.scrollWheel({0, 2}, {})
+        end,nil,
+        function()
+            hs.eventtap.scrollWheel({0, 2}, {})
+        end)
+
+        --Disbale Scroll Right Hotkey
+        hs.hotkey.disableAll({}, "L",
+        function()
+            hs.eventtap.scrollWheel({-2, 0}, {})
+        end,nil,
+        function()
+            hs.eventtap.scrollWheel({-2, 0}, {})
+        end)
+    end
+end
+
+init()
+
+--TODO: Add more functionality?
+-- Add more vim navigation commands
