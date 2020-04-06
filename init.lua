e11b9bdfc4ce06f187a237669d1e0da258561a98
emanuel2718 <eramirez2718@gmail.com>
2020-04-06 02:15:34 -0400

    Add scrolling speed variable

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 --Author: Emanuel Ramirez
 --Date: 04/05/2020
 
+scrollingSped = 2
+
 function init()
     appsWatcher = hs.application.watcher.new(previewWatcher)
     appsWatcher:start()
@@ -13,37 +15,37 @@ function previewWatcher(name, event, app)
         --Scroll Left
         hs.hotkey.bind({}, "H",
         function()
-            hs.eventtap.scrollWheel({2, 0}, {})
+            hs.eventtap.scrollWheel({scrollingSped, 0}, {})
         end,nil,
         function()
-            hs.eventtap.scrollWheel({2, 0}, {})
+            hs.eventtap.scrollWheel({scrollingSped, 0}, {})
         end)
 
         --Scroll Down
         hs.hotkey.bind({}, "J",
         function()
-            hs.eventtap.scrollWheel({0, -2}, {})
+            hs.eventtap.scrollWheel({0, -scrollingSped}, {})
         end,nil,
         function()
-            hs.eventtap.scrollWheel({0, -2}, {})
+            hs.eventtap.scrollWheel({0, -scrollingSped}, {})
         end)
 
         --Scroll Up
         hs.hotkey.bind({}, "K",
         function()
-            hs.eventtap.scrollWheel({0, 2}, {})
+            hs.eventtap.scrollWheel({0, scrollingSped}, {})
         end,nil,
         function()
-            hs.eventtap.scrollWheel({0, 2}, {})
+            hs.eventtap.scrollWheel({0, scrollingSped}, {})
         end)
 
         --Scroll Right
         hs.hotkey.bind({}, "L",
         function()
-            hs.eventtap.scrollWheel({-2, 0}, {})
+            hs.eventtap.scrollWheel({-scrollingSped, 0}, {})
         end,nil,
         function()
-            hs.eventtap.scrollWheel({-2, 0}, {})
+            hs.eventtap.scrollWheel({-scrollingSped, 0}, {})
         end)
     end
 
@@ -53,42 +55,39 @@ function previewWatcher(name, event, app)
         --Disbale Scroll Left Hotkey
         hs.hotkey.disableAll({}, "H",
         function()
-            hs.eventtap.scrollWheel({2, 0}, {})
+            hs.eventtap.scrollWheel({scrollingSped, 0}, {})
         end,nil,
         function()
-            hs.eventtap.scrollWheel({2, 0}, {})
+            hs.eventtap.scrollWheel({scrollingSped, 0}, {})
         end)
        
         --Disbale Scroll Down Hotkey
         hs.hotkey.disableAll({}, "J",
         function()
-            hs.eventtap.scrollWheel({0, -2}, {})
+            hs.eventtap.scrollWheel({0, -scrollingSped}, {})
         end,nil,
         function()
-            hs.eventtap.scrollWheel({0, -2}, {})
+            hs.eventtap.scrollWheel({0, -scrollingSped}, {})
         end)
 
         --Disbale Scroll Up Hotkey
         hs.hotkey.disableAll({}, "K",
         function()
-            hs.eventtap.scrollWheel({0, 2}, {})
+            hs.eventtap.scrollWheel({0, scrollingSped}, {})
         end,nil,
         function()
-            hs.eventtap.scrollWheel({0, 2}, {})
+            hs.eventtap.scrollWheel({0, scrollingSped}, {})
         end)
 
         --Disbale Scroll Right Hotkey
         hs.hotkey.disableAll({}, "L",
         function()
-            hs.eventtap.scrollWheel({-2, 0}, {})
+            hs.eventtap.scrollWheel({-scrollingSped, 0}, {})
         end,nil,
         function()
-            hs.eventtap.scrollWheel({-2, 0}, {})
+            hs.eventtap.scrollWheel({-scrollingSped, 0}, {})
         end)
     end
 end
 
 init()
-
---TODO: Add more functionality?
--- Add more vim navigation commands
